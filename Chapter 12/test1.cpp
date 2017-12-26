c24afa70c7960db3dafdffc8df07865441dea006
Alexander Hartson <alexander-hartson@live.com>
2017-12-26 00:00:37 -0600

    code fix

--- Chapter 12/test1.cpp
+++ Chapter 12/test1.cpp
@@ -1,1 +1,3 @@
-#include "Simple_window.h" 
+#include "std_lib_facilities.h"
+#include "Simple_window.h" //get access to our window library
+#include "Graph.h"  //get access to our grapihcs library facilities
