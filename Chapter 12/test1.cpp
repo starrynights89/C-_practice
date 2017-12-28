cfe0380c35dc2e4e6a492e4e4f8dd9e87e6e6f4b
Alexander Hartson <alexander-hartson@live.com>
2017-12-27 20:57:07 -0600

    adding code

--- Chapter 12/test1.cpp
+++ Chapter 12/test1.cpp
@@ -8,8 +8,16 @@ int main()
 	Point tl(100, 100); //to become top left corner of window
 	Simple_window win(tl, 600, 400, "Canvas"); //make a simple window
 
+    //x Axis
     Axis xa {Axis::x, Point{20,300},280,10,"x axis"}; //make an Axis
     win.attach(xa); //attach xa to the window, win
     win.set_label("Canvas #2"); //relabel the window
+
+    //y Axis
+    Axis ya {Axis::y, Point{20,300},280,10,"y axis"};
+    ya.set_color(Color::cyan); //chose a color
+    ya.label.set_color(Color::dark_red); //choose a color for the text
+    win.set_label("Canvas #3"); //display!
+
 	win.wait_for_button(); //give control to the display engine
 }
