901de3baf20e92ddcca9f9e231ab9f288fb829fa
Alexander Hartson <alexander-hartson@live.com>
2017-12-27 20:27:58 -0600

    adding code

--- Chapter 12/test1.cpp
+++ Chapter 12/test1.cpp
@@ -6,8 +6,10 @@ int main()
 	using namespace Graph_lib; //our graphics facilities are in Graph_lib
 
 	Point tl(100, 100); //to become top left corner of window
-
 	Simple_window win(tl, 600, 400, "Canvas"); //make a simple window
 
+    Axis xa {Axis::x, Point{20,300},280,10,"x axis"}; //make an Axis
+    win.attach(xa); //attach xa to the window, win
+    win.set_label("Canvas #2"); //relabel the window
 	win.wait_for_button(); //give control to the display engine
 }
