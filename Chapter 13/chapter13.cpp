fd98ad8263dfa6db0da9744ca4b5b90ce3aec97f
Alexander Hartson <alexander-hartson@live.com>
2017-12-30 14:17:43 -0600

    bug fix

--- Chapter 13/chapter13.cpp
+++ Chapter 13/chapter13.cpp
@@ -1,30 +1,33 @@
 #include "Graph.h"
 #include "Simple_window.h"
-#include "Point.h"
+
 
 struct Point
 {
     int x, y;
 };
 
-bool operator==(Point a, Point b) { return a.x==b.x && a.y==b.y; }
-bool operator!=(Point a, Point b) { return !(a==b); }
+bool operator==(Graph_lib::Point a, Graph_lib::Point b) { return a.x==b.x && a.y==b.y; }
+bool operator!=(Graph_lib::Point a, Graph_lib::Point b) { return !(a == b); }
 
 //a Line is a Shape defined by two Points
 struct Line : Shape 
 {
-    Line(Point p1, Point p2) //construct a Line from two Points
+	Line(Graph_lib::Point p1, Graph_lib::Point p2); //construct a Line from two Points
 };
 
-//draw two lines
-constexpr Point x {100,100};
+int main()
+{
+	//draw two lines
+	Graph_lib::Point x{ 100,100 };
 
-Simple_window win1 {x,600,400,"two lines"};
+	Simple_window win1(x, 600, 400, "two lines");
 
-Line horizontal {x,Point{200,100}}; //make a horizontal line
-Line vertical {Point{150,50},Point{150,150}}; //make a vertical line
+	Graph_lib::Line horizontal{ x,Graph_lib::Point{ 200,100 } }; //make a horizontal line
+	Graph_lib::Line vertical{ Graph_lib::Point{ 150,50 },Graph_lib::Point{ 150,150 } }; //make a vertical line
 
-win1.attach(horizontal); //attack the lines to the window
-win1.attach(vertical); 
+	win1.attach(horizontal); //attack the lines to the window
+	win1.attach(vertical);
 
-win1.wait_for_button(); //display!
+	win1.wait_for_button(); //display!
+}
