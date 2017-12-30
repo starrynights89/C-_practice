78cb34188dbb65e2f3f7bae39751441f0c8fb9a3
Alexander Hartson <alexander-hartson@live.com>
2017-12-30 13:43:02 -0600

    adding code

--- Chapter 13/chapter13.cpp
+++ Chapter 13/chapter13.cpp
@@ -15,3 +15,16 @@ struct Line : Shape
 {
     Line(Point p1, Point p2) //construct a Line from two Points
 };
+
+//draw two lines
+constexpr Point x {100,100};
+
+Simple_window win1 {x,600,400,"two lines"};
+
+Line horizontal {x,Point{200,100}}; //make a horizontal line
+Line vertical {Point{150,50},Point{150,150}}; //make a vertical line
+
+win1.attach(horizontal); //attack the lines to the window
+win1.attach(vertical); 
+
+win1.wait_for_button(); //display!
