33b73bdfa85841bffb70440207c3dd6da8e7159b
Alexander Hartson <alexander-hartson@live.com>
2017-12-30 13:25:17 -0600

    adding code

--- Chapter 13/chapter13.cpp
+++ Chapter 13/chapter13.cpp
@@ -1,2 +1,11 @@
 #include "Graph.h"
 #include "Simple_window.h"
+#include "Point.h"
+
+struct Point
+{
+    int x, y;
+}
+
+bool operator==(Point a, Point b) { return a.x==b.x && a.y==b.y; }
+bool operator!=(Point a, Point b) { return !(a==b); }
