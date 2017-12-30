b3466070d0018c8ec89bd36ded859c5f64f3cf2a
Alexander Hartson <alexander-hartson@live.com>
2017-12-30 13:39:36 -0600

    adding code

--- Chapter 13/chapter13.cpp
+++ Chapter 13/chapter13.cpp
@@ -5,7 +5,13 @@
 struct Point
 {
     int x, y;
-}
+};
 
 bool operator==(Point a, Point b) { return a.x==b.x && a.y==b.y; }
 bool operator!=(Point a, Point b) { return !(a==b); }
+
+//a Line is a Shape defined by two Points
+struct Line : Shape 
+{
+    Line(Point p1, Point p2) //construct a Line from two Points
+};
