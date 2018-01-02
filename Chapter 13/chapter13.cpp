d8bb34a622fdf7479601601df045cb3266818191
Alexander Hartson <alexander-hartson@live.com>
2018-01-02 11:29:11 -0600

    code fix

--- Chapter 13/chapter13.cpp
+++ Chapter 13/chapter13.cpp
@@ -9,22 +9,22 @@ struct Point
 bool operator==(Graph_lib::Point a, Graph_lib::Point b) { return a.x==b.x && a.y==b.y; }
 bool operator!=(Graph_lib::Point a, Graph_lib::Point b) { return !('a'=='b'); }
 
-struct Lines : Shape //related lines
+struct Line : Shape //related lines
 {
-	Lines() {} //empty
-	Lines(initializer_list<Graph_lib::Point>lst); //initialize from a list of points
-
-	void draw_lines() const;
-	void add(Graph_lib::Point p1, Graph_lib::Point p2);
+	Line(Graph_lib::Point p1, Graph_lib::Point p2);
 };
 
 int main()
 {
-	using namespace Graph_lib;
-	//draw two lines
+	const Graph_lib::Point x{ 100,100 };
 
-	Graph_lib::Lines x;
-	x.add(Graph_lib::Point{100,100},Graph_lib::Point{200,100}); //horizontal
-	x.add(Graph_lib::Point{150,50},Graph_lib::Point{150,150}); //vertical\
-	
+	Simple_window win1{x, 600,400,"two lines" };
+
+	Graph_lib::Line horizontal{ x,Graph_lib::Point{200,100} };
+	Graph_lib::Line vertical{ Graph_lib::Point{150,50},Graph_lib::Point{150,150} };
+
+	win1.attach(horizontal);
+	win1.attach(vertical);
+
+	win1.wait_for_button();
 }
