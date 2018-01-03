17e0b2d600083b7f8539643571593f650b934b14
Alexander Hartson <alexander-hartson@live.com>
2018-01-03 11:28:24 -0600

    adding code

--- Chapter 13/chapter13.cpp
+++ Chapter 13/chapter13.cpp
@@ -101,12 +101,21 @@ int main()
 
 	//rectangle
 	Graph_lib::Rectangle rect00{Graph_lib::Point{150,100},200,100};
-	Graph_lib::Rectangle rect
+	Graph_lib::Rectangle rect11{Graph_lib::Point{50,50},Graph_lib::Point{250,150}};
+	Graph_lib::Rectangle rect12{Graph_lib::Point{50,150},Point{250,250}}; //just below rect11
+	Graph_lib::Rectangle rect21{Graph_lib::Point{250,50},200,100}; //just to the right of rect 11
+	Graph_lib::Rectangle rect22{Graph_lib::Point{250,150},200,100}; //just below rect 21
+
+	rect00.set_fill_color(Color::yellow);
+	rect11.set_fill_color(Color::blue);
+	rect12.set_fill_color(Color::red);
+	rect21.set_fill_color(Color::green);
 
 	//win.attach(x);
 	//win.attach(grid);
 	//win.attach(opl);
 	//win.attach(cpl);
 	//win.attach(poly);
+	win.attach(rect00);
 	win.wait_for_button();
 }
