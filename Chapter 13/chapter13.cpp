5a9b36322d26d5186d6b2b8c5b9707c3294c6816
Alexander Hartson <alexander-hartson@live.com>
2018-01-05 09:59:26 -0600

    code fix

--- Chapter 13/chapter13.cpp
+++ Chapter 13/chapter13.cpp
@@ -107,14 +107,14 @@ int main()
 	rect21.set_color(Color::invisible);
 	rect22.set_color(Color::invisible);
 
-	Vector_ref<Rectangle>vr;
+	Vector_ref<Graph_lib::Rectangle>vr;
 	for(int i=0; i<16; i++)
 	{
 		for(int j=0; j<16; j++)
 		{
-			vr.push_back(new Rectangle{Point{i*20,j*20},20,20});
+			vr.push_back(new Graph_lib::Rectangle{Graph_lib::Point{i*20,j*20},20,20});
 			vr[vr.size()-1].set_fill_color(Color{i*16+j});
-			win20.attach(vr[vr.size()-1]);
+			win.attach(vr[vr.size()-1]);
 		}
 	}
 	//win.attach(x);
@@ -127,6 +127,5 @@ int main()
 	//win.attach(rect12);
 	//win.attach(rect21);
 	//win.attach(rect22);
-	win.attacg(vr);
 	win.wait_for_button();
 }
