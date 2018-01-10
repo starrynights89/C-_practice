1b289f96a6eb1d186e6f7e2bfd443db39ea42323
Alexander Hartson <alexander-hartson@live.com>
2018-01-10 11:25:14 -0600

    adding code

--- Chapter 13/drill.cpp
+++ Chapter 13/drill.cpp
@@ -3,7 +3,12 @@
 
 int main()
 {
+    //adding window
     Simple_window win(Graph_lib::Point(100,100), 800, 1000, "Grid");
+
+    //adding grid
+    Graph_lib::Lines grid;
+    
 
     win.wait_for_button();
 }
