1f81c911fed8870832e1fa9bc1310b1289ecde2d
Alexander Hartson <alexander-hartson@live.com>
2018-01-10 12:19:43 -0600

    adding code

--- Chapter 13/drill.cpp
+++ Chapter 13/drill.cpp
@@ -2,7 +2,6 @@
 #include "Simple_window.h"
 
 int main()
-try
 {
     //adding window
     Simple_window win(Graph_lib::Point(100,100), 800, 1000, "Grid");
@@ -20,15 +19,4 @@ try
     win.attach(grid);
     //win.resize(1000,800);
     win.wait_for_button();
-}
-
-catch (exception& e)
-{
-    cerr << "exception: " << e.what() << endl;
-    keep_window_open();
-}
-catch (...)
-{
-    cerr << "exception\n";
-    keep_window_open();
 }
