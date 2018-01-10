c88dd6f4324e3942297e2ab168fa4e4feab5e347
Alexander Hartson <alexander-hartson@live.com>
2018-01-10 12:54:19 -0600

    adding code

--- Chapter 13/drill.cpp
+++ Chapter 13/drill.cpp
@@ -10,10 +10,19 @@ int main()
     Graph_lib::Lines grid;
     int x_size = 800;
     int y_size = 800;
-    for (int i = 100; i<=x_size; i+=100)
+    for (int i=100; i<=x_size; i+=100)
     {
         grid.add(Point(i,0),Point(i,y_size));
         grid.add(Point(0,i),Point(x_size,i));
+    }
+
+    //make squares
+    vector_ref<Graph_lib::Rectangle>vr;
+    for (int i=0; i<8; i++)
+    {
+        vr.push_back(new Graph_lib::Rectangle(Point(i*100,i*100),101,101));
+        vr[vr.size()-1].set_fill_color(Color::red);
+        win.attach(vr[vr.size()-1]);
     }
 
     win.attach(grid);
