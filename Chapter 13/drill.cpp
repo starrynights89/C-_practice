0666f2545f3b85040c57f6c8f8c86a6806213a58
Alexander Hartson <alexander-hartson@live.com>
2018-01-10 15:12:49 -0600

    bug fix

--- Chapter 13/drill.cpp
+++ Chapter 13/drill.cpp
@@ -4,7 +4,8 @@
 int main()
 {
     //adding window
-    Simple_window win(Point(100,100), 800, 1000, "Grid");
+    Simple_window win(Point(300,50), 800, 1000, "Grid");
+    win.wait_for_button();
 
     //adding grid
     Lines grid;
@@ -15,6 +16,8 @@ int main()
         grid.add(Point(i,0),Point(i,y_size));
 		grid.add(Point(0, i),Point(x_size, i));
     }
+    win.attach(grid);
+    win.wait_for_button();
 
     //make squares that are red along a vertical line
     Vector_ref<Graph_lib::Rectangle>vr;
@@ -24,18 +27,26 @@ int main()
         vr[vr.size()-1].set_fill_color(Color::red);
         win.attach(vr[vr.size()-1]);
     }
+    win.wait_for_button();
 
     //place 3 copies of a 200x200 image, don't cover the red squares
     Image plane1(Point(200,0),"C://Users//alexa//source//repos//"
 	"chapter13_practice c++//Release//image.jpg");
+    win.attach(plane1);
     Image plane2(Point(400,200),"C://Users//alexa//source//repos//"
 	"chapter13_practice c++//Release//image.jpg");
+    win.attach(plane2);
     Image plane3(Point(600,400),"C://Users//alexa//source//repos//"
 	"chapter13_practice c++//Release//image.jpg");
+    win.attach(plane3);
+    win.wait_for_button();
 
     //place 100x100 that moves with clicking next
-    Image anime(Point(0,0),"C://Users//alexa//source//repos//"
+    /*
+	Image anime(Point(0,0),"C://Users//alexa//source//repos//"
 	"chapter13_practice c++//Release//image1.png");
+	anime.set_mask(Point(110, 70), 100, 100);
+	//win.attach(anime);
 
     int x = 0;
     int y = 0;
@@ -48,13 +59,9 @@ int main()
         dx = 100*x - anime.point(0).x;
         dy = 100*y - anime.point(0).y;
         anime.move(dx,dy);
+		win.wait_for_button();
     }
+	*/
 
-    win.attach(grid);
-    win.resize(1000,800);
-    win.attach(plane1);
-	win.attach(plane2);
-	win.attach(plane3);
-    win.attach(anime);
-    win.wait_for_button();
+    //win.resize(1000,800);
 }
