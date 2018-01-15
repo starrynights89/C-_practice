0e8112f0c32c091b6eaa7aa2a76305910b47aecf
Alexander Hartson <alexander-hartson@live.com>
2018-01-15 15:18:22 -0600

    adding code

--- Chapter 14/drill.cpp
+++ Chapter 14/drill.cpp
@@ -1,3 +1,22 @@
 //Chapter 14 drill
 
 #include "std_lib_facilities.h"
+#include "Simple_window.h"
+#include "Graph.h"
+
+class B1
+{
+public:
+    virtual void vf() { cout << "B1::vf()\n"; }
+    void f() { cout << "B1::f()\n"; }
+    //virtual void pvf() =0;
+};
+
+int main()
+{
+    B1 b1;
+    b1.vf();
+    b1.f();
+
+    keep_window_open();
+}
