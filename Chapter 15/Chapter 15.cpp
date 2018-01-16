b90ef9d25712031be15f8275c7b03da3fd83015d
Alexander Hartson <alexander-hartson@live.com>
2018-01-16 00:16:12 -0600

    bug fix

--- Chapter 15/Chapter 15.cpp
+++ Chapter 15/Chapter 15.cpp
@@ -12,7 +12,7 @@ int main()
 
     const int x_orig = xmax/2; //position of (0,0) is center of window
     const int y_orig = ymax/2;
-    const Point orig {x_orig,y_orig};
+    const Point orig(x_orig,y_orig);
 
     const int r_min = -10; //range [-10:11)
     const int r_max = 11; 
@@ -24,12 +24,15 @@ int main()
 
     Simple_window win {Point{100,100},xmax,ymax,"Function graphing"};
 
-    Function s {one,r_min,r_max,orig,n_points,x_scale,y_scale};
-    Function s2 {slope,r_min,r_max,orig,n_points,x_scale,y_scale};
-    Function s3 {square,r_min,r_max,orig,n_points,x_scale,y_scale};
-
+    Function s(one,r_min,r_max,orig,n_points,x_scale,y_scale);
     win.attach(s);
+    win.wait_for_button();
+
+    Function s2(slope,r_min,r_max,orig,n_points,x_scale,y_scale);
     win.attach(s2);
+    win.wait_for_button();
+
+    Function s3(square,r_min,r_max,orig,n_points,x_scale,y_scale);
     win.attach(s3);
     win.wait_for_button();
 }
