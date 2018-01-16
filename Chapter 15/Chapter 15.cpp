783a9d908243ad8f7450570ee985bff373fa27f9
Alexander Hartson <alexander-hartson@live.com>
2018-01-15 23:32:40 -0600

    adding code

--- Chapter 15/Chapter 15.cpp
+++ Chapter 15/Chapter 15.cpp
@@ -8,5 +8,15 @@ int main()
 
     constexpr int x_orig = xmax/2; //position of (0,0) is center of window
     constexpr int y_orig = ymax/2;
-    constexpr Point orig {x_}
+    constexpr Graph_lib::Point orig {x_orig,y_orig};
+
+    constexpr int r_min = -10; //range [-10:11)
+    constexpr int r_max = 11; 
+
+    constexpr int n_points = 400; //number of points used in range
+
+    constexpr int x_scale = 30; //scaling factors
+    constexpr int y_scale = 30;
+
+    Simple_window win {Point{100,100},xmax,ymax,"Function graphing"};
 }
