5c9da386eeec1cd1a57d2dfef8bbb8887065da1e
Alexander Hartson <alexander-hartson@live.com>
2018-01-15 23:35:22 -0600

    adding code

--- Chapter 15/Chapter 15.cpp
+++ Chapter 15/Chapter 15.cpp
@@ -8,7 +8,7 @@ int main()
 
     constexpr int x_orig = xmax/2; //position of (0,0) is center of window
     constexpr int y_orig = ymax/2;
-    constexpr Graph_lib::Point orig {x_orig,y_orig};
+    const Graph_lib::Point orig {x_orig,y_orig};
 
     constexpr int r_min = -10; //range [-10:11)
     constexpr int r_max = 11; 
@@ -19,4 +19,13 @@ int main()
     constexpr int y_scale = 30;
 
     Simple_window win {Point{100,100},xmax,ymax,"Function graphing"};
+
+    Function s {one,r_min,r_max,orig,n_points,x_scale,y_scale};
+    Function s2 {slope,r_min,r_max,orig,n_points,x_scale,y_scale};
+    Function s3 {square,r_min,r_max,orig,n_points,x_scale,y_scale};
+
+    win.attach(s);
+    win.attach(s2);
+    win.attach(s3);
+    win.wait_for_button();
 }
