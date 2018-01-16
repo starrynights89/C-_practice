9778d4912b4833263f9ad934900524df6cae8365
Alexander Hartson <alexander-hartson@live.com>
2018-01-15 22:44:29 -0600

    adding code

--- Chapter 15/Chapter 15.cpp
+++ Chapter 15/Chapter 15.cpp
@@ -3,5 +3,10 @@
 
 int main()
 {
-    
+    constexpr int xmax = 600; //window size
+    constexpr int ymax = 400; 
+
+    constexpr int x_orig = xmax/2; //position of (0,0) is center of window
+    constexpr int y_orig = ymax/2;
+    constexpr Point orig {x_}
 }
