54da801b69a0331ed64f7d97f27a82fc97c4fc77
Alexander Hartson <alexander-hartson@live.com>
2018-01-18 11:28:05 -0600

    adding data

--- Chapter 15/Chapter 15_age_japan.cpp
+++ Chapter 15/Chapter 15_age_japan.cpp
@@ -1,2 +1,16 @@
 #include "Graph.h"
 #include "Simple_window.h"
+
+struct Distribution
+{
+    int year, young, middle, old;
+};
+
+istream& operator>>(istream& is, Distribution& d)
+    //assume format: (year : young middle old)
+{
+    char ch1 = 0;
+    char ch2 = 0;
+    char ch3 = 0;
+    Distribution dd;
+}
