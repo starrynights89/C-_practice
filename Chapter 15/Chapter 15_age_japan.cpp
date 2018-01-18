9391feba6d69acad9a0d83b2f75eff64e8ef60ab
Alexander Hartson <alexander-hartson@live.com>
2018-01-18 11:33:55 -0600

    adding code

--- Chapter 15/Chapter 15_age_japan.cpp
+++ Chapter 15/Chapter 15_age_japan.cpp
@@ -13,4 +13,21 @@ istream& operator>>(istream& is, Distribution& d)
     char ch2 = 0;
     char ch3 = 0;
     Distribution dd;
+
+    if(is >> ch1 >> dd.year
+          >> ch2 >> dd.young >> dd. middle >> dd.old
+          >> ch3)
+    {
+        if(ch1!='('||ch2!=':'||ch3!=')')
+        {
+            is.clear(ios_base::failbit);
+            return is;
+        }
+    }
+    else
+    {
+        return is;
+    }
+    d = dd;
+    return is;
 }
