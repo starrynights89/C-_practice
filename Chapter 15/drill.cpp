b6cdaa330242b04c56d247bf23db6dc8fb326cb5
Alexander Hartson <alexander-hartson@live.com>
2018-01-21 17:30:23 -0600

    bug fix

--- Chapter 15/drill.cpp
+++ Chapter 15/drill.cpp
@@ -2,9 +2,9 @@
 #include "Simple_window.h"
 #include "Window.h"
 
-int main
+int main()
 {
-    Window.win(Point(100,100),600,600,"Function Graphs");
+	Simple_window win(Point(100,100),600,600,"Function Graphs");
 
     win.wait_for_button();
 }
