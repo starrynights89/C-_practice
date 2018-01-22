16137e2da3d6c6cdd924cf69f6719e112b04a707
Alexander Hartson <alexander-hartson@live.com>
2018-01-21 18:55:33 -0600

    adding code

--- Chapter 15/drill.cpp
+++ Chapter 15/drill.cpp
@@ -11,6 +11,14 @@ int main()
     constexpr int y_orig = ymax/2;
     const Point orig(x_orig,y_orig);
 
+    constexpr int r_min = -10; //range [-10:11)
+    constexpr int r_max = 11; 
+
+    constexpr int n_points = 400; //number of points used in range
+
+    constexpr int x_scale = 20; //scaling factors
+    constexpr int y_scale = 20;
+
 	Graph_lib::Window win(Point(100,100),xmax,ymax,"Function Graphs");
 
     gui_main();
