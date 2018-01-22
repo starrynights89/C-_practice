57e4061a689b008a8c710726451dfb6f0092bd78
Alexander Hartson <alexander-hartson@live.com>
2018-01-21 18:39:43 -0600

    adding code

--- Chapter 15/drill.cpp
+++ Chapter 15/drill.cpp
@@ -4,7 +4,14 @@
 
 int main()
 {
-	Graph_lib::Window win(Point(100,100),600,600,"Function Graphs");
+    constexpr int xmax = 600; //window size
+    constexpr int ymax = 600; 
+
+    constexpr int x_orig = xmax/2; //position of (0,0) is center of window
+    constexpr int y_orig = ymax/2;
+    const Point orig(x_orig,y_orig);
+
+	Graph_lib::Window win(Point(100,100),xmax,ymax,"Function Graphs");
 
     gui_main();
 }
