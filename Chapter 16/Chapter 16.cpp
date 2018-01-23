2510db712d8a62a6d28f391259eb0376d0962b06
Alexander Hartson <alexander-hartson@live.com>
2018-01-23 17:08:35 -0600

    adding struct

--- Chapter 16/Chapter 16.cpp
+++ Chapter 16/Chapter 16.cpp
@@ -1,2 +1,14 @@
 #include "Simple_window.h"
 #include "Graph.h"
+
+struct Simple_window : Graph_lib::Window
+{
+    Simple_window(Point xy,int w,int h,const string & title);
+    void wait_for_button(); //simple event loop
+private:
+    Button next_button; //the "Next" button
+    bool button_pushed; //implementation detail
+
+    static void cb_next(Address,Address); //callback for next_button
+    void next(); //action to be done when next_button is pressed
+};
