915bd9df24701e5a52236b65c64e0ca7ad6ffb5a
Alexander Hartson <alexander-hartson@live.com>
2018-01-22 20:38:14 -0600

    adding code

--- Chapter 15/drill_class.cpp
+++ Chapter 15/drill_class.cpp
@@ -4,9 +4,21 @@
 
 struct Person
 {
-    Person() : first_n(""), second_n(""), a(0) { }
-    Person(string fn, string sn, int age) :first_n(fn), second_n(sn), a(age)
+	Person() : first_n(""), second_n(""), a(0) { }
+	Person(string fn, string sn, int age) :first_n(fn), second_n(sn), a(age) { }
+    string first_name() const { return first_n; }
+    string second_name() const { return second_n; }
+    int age() const { return a; }
+private:
+    string first_n;
+    string second_n;
+    int a;
 };
+
+istream& operator>>(istream& is, Person& p)
+{
+    string fname, sname;
+}
 
 int main()
 {
