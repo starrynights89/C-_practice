a3645cad69b8ec26c7d8491e784b30dc8a137495
Alexander Hartson <alexander-hartson@live.com>
2018-01-22 20:33:34 -0600

    adding code

--- Chapter 15/drill_class.cpp
+++ Chapter 15/drill_class.cpp
@@ -2,6 +2,12 @@
 #include "Simple_window.h"
 #include "Window.h"
 
+struct Person
+{
+    Person() : first_n(""), second_n(""), a(0) { }
+    Person(string fn, string sn, int age) :first_n(fn), second_n(sn), a(age)
+};
+
 int main()
 {
 }
