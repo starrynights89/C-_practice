832517b2f34284994e441c935ab93744e69b8475
Alexander Hartson <alexander-hartson@live.com>
2018-01-22 22:33:58 -0600

    code change

--- Chapter 15/drill_class.cpp
+++ Chapter 15/drill_class.cpp
@@ -1,84 +1,63 @@
-#include "Graph.h"
 #include "Simple_window.h"
-#include "Window.h"
+#include "Graph.h"
 
-struct Person
-{
-	Person() : first_n(""), second_n(""), a(0) { }
-	Person(string fn, string sn, int age) :first_n(fn), second_n(sn), a(age)
-    {
-        const string ill_chars = ";:\"'[]*&^%$#@!";
-        for(int i=0; i<fn.size(); i++)
-        {
-            for(int j=0; j<ill_chars.size(); j++)
-            {
-                if(fn[i]==ill_chars[j]) error("illegal character: ",string(1,fn[i]));
-            }
-        }
-        for(int i=0; i<sn.size(); i++)
-        {
-            for(int j=0; j<ill_chars.size(); j++)
-            {
-                if(sn[i]==ill_chars[j]) error("illegal character: ",string(1,sn[i]));
-            }
-        }
-        if(a<0 || a>=150) error("age must be in range [0,150)");
+class Person {
+    string first, second;
+    int years;
+
+    bool check (string n) {
+        if (int(n.size()) < 1 || isspace(n[0]) || isspace(n[n.size()-1]))
+            return false;
+        bool space = false;
+        for (int i = 0; i < int(n.size()); ++i)
+            if (! space && isspace(n[i]))
+                space = true;
+            else if (! isalnum(n[i]) && n[i] != '-' && n[i] != '_' &&
+                    n[i] != '.' && n[i] != ',' &&  n[i] != '/' &&
+                    n[i] != '(' && n[i] != ')')
+                return false;
+        return space;
     }
-    string first_name() const { return first_n; }
-    string second_name() const { return second_n; }
-    int age() const { return a; }
-private:
-    string first_n;
-    string second_n;
-    int a;
+public:
+    Person(string n, int a) : years(a) {
+        if (! check(n)) error("That isn't a name");
+        string s;
+        for (int i = 0; i < int(n.size()); ++i)
+            if (isspace(n[i])) {
+                first = s;
+                s = "";
+            } else
+                s += n[i];
+        second = s;
+        if (years < 0 || years > 150) error ("That isn't an age");
+    }
+    Person () : first("Anonnie"), second("Mouse"), years(999) {}
+    Person (const Person & arg) :
+        first(arg.first), second(arg.second), years(arg.years) {}
+    const string name () const {return first+" "+second;}
+    const int age () const {return years;}
 };
 
-istream& operator>>(istream& is, Person& p)
-{
-    string fname, sname;
-    int age;
-    is >> fname >> sname >> age;
-    if (!is) return is;
-    p = Person(fname,sname,age);
-    return is;
+istream & operator>> (istream & stream, Person & arg) {
+    string s1, s2;
+    int a;
+    stream >> s1 >> s2 >> a;
+    if (! stream) return stream;
+    Person p(s1+" "+s2,a);
+    arg = p;
+    return stream;
 }
 
-ostream& operator<<(ostream& os, const Person& p)
-{
-    return os << "Name: " << p.first_name() << ' ' 
-        << p.second_name() << ", age: " << p.age();
+ostream & operator<< (ostream & stream, const Person & arg) {
+    stream << arg.name() << " is " << arg.age();
+    return stream;
 }
 
-int main()
-{
-    /*
+int main () {
+    vector<Person> gnomes;
+    gnomes.push_back(Person("Goofy Dawg",63));
     Person p;
-    p.n = "Goofy";
-    p.a = 63;
-    cout << "Name: " << p.n << ", age: " << p.a << endl;
-    */
-
-    cout << "Enter name and age: ";
-    Person p2;
-    cin >> p2;
-    cout << p2 << endl;
-
-    Person p3;
-    while (cin>>p3)
-    {
-        cout << p3 << endl;
-    }
-
-    vector<Person> persons;
-    Person p4;
-    while (cin>>p4)
-    {
-        persons.push_back(p4);
-    }
-    for(int i=0; i<persons.size(); i++)
-    {
-        cout << persons[i] << endl;
-    }
-
-    keep_window_open("~");
+    while (cin >> p) gnomes.push_back(p);
+    for (int i = 0; i < int(gnomes.size()); ++i)
+        cout << gnomes[i] << endl;
 }
