c25d5647623e2349c385a5abea4b9207863722bb
Alexander Hartson <alexander-hartson@live.com>
2018-01-24 16:47:06 -0600

    adding code

--- Chapter 16/drill.cpp
+++ Chapter 16/drill.cpp
@@ -35,6 +35,7 @@ private:
     static void cb_red(Address, Address); 
     static void cb_blue(Address, Address);
     static void cb_black(Address, Address);
+    static void cb_menu(Address, Address)
 	static void cb_next(Address, Address);
 	static void cb_quit(Address, Address);
 };
