8130529f94ae09db01190e7696de6807b77fa10d
Alexander Hartson <alexander-hartson@live.com>
2018-01-24 12:17:38 -0600

    bug fix

--- Chapter 16/drill.cpp
+++ Chapter 16/drill.cpp
@@ -20,11 +20,22 @@ private:
 
     void next();
     void quit();
+
+	// callback functions
+	static void cb_next(Address, Address);
+	static void cb_quit(Address, Address);
 };
 
 //add constructor
 Lines_window::Lines_window(Point xy,int w,int h,const string& title)
     :Window(xy,w,h,title),
     next_button(Point(x_max()-150,0),70,20,"Next point",
-        [](Adress,Address pw) { reference_to<Lines_window>(pw).next(); },
-    quit_button(Point())
+        [](Address,Address pw) { reference_to<Lines_window>(pw).next(); },
+    quit_button(Point(x_max()-70,0),70,20,"Quit,",
+        [](Address,Address pw) { reference_to<Lines_window>(pw).quit(); },
+    next_x(Point(x_max()-310,0),50,20,"next x:"),
+    next_y(Point(x_max()-210,0),50,20,"next y:"),
+    xy_out(Point(100,0),100,20,"current (x,y:")
+{
+    attach
+}
