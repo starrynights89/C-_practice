1c8177e3a029a209a5fc0617a5dfbf39c8d0845b
Alexander Hartson <alexander-hartson@live.com>
2018-01-24 14:38:37 -0600

    adding error detection

--- Chapter 16/drill.cpp
+++ Chapter 16/drill.cpp
@@ -74,7 +74,17 @@ void Lines_window::next()
 }
 
 int main()
-{
+try {
     Lines_window win(Point(100,100),600,400,"lines");
     return gui_main();
+}
+catch(exception& e)
+{
+    cerr << "exception: " << e.what() << '\n';
+    return 1;
+}
+catch(...)
+{
+    cerr << "Some exception\n";
+    return 2;
 }
