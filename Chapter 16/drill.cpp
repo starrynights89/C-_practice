fe993d5aaf30813e709a19de13e6ab3e898f98f9
Alexander Hartson <alexander-hartson@live.com>
2018-01-23 23:53:01 -0600

    adding code

--- Chapter 16/drill.cpp
+++ Chapter 16/drill.cpp
@@ -1,2 +1,22 @@
+/*
+This program has the user put in x and y
+coordinates to draw shapes
+*/
+
 #include "Simple_window.h"
 #include "Graph.h"
+
+struct Lines_window : Window
+{
+    Lines_window(Point xy,int w,int h,const string& title);
+    Open_polyline lines;
+private:
+    Button next_button; //add (next_x,next_y) to lines
+    Button quit_button;
+    In_box next_x;
+    In_box next_y;
+    Out_box xy_out;
+
+    void next();
+    void quit();
+};
