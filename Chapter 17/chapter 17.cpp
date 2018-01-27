cc68a9060efea83d976cfe27a9af96a94621321e
Alexander Hartson <alexander-hartson@live.com>
2018-01-26 20:37:47 -0600

    Update chapter 17.cpp

--- Chapter 17/chapter 17.cpp
+++ Chapter 17/chapter 17.cpp
@@ -11,3 +11,10 @@ public:
 				   //store s in sz
 	int size() const { return sz; } //the current size
 };
+
+void sizes(char ch, int i, int* p)
+{
+	cout << "the size of char is " << sizeof(char) << '' << sizeof(ch) << '\n';
+	cout << "the size of int is " << sizeof(int) << '' << sizeof(i) << '\n';
+	cout << "the size of int* is " << sizeof(int*) << '' << sizeof(p) << '\n';
+}
