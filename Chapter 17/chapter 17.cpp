168ebc52c0cc72b849dfbc34b8a622565b8b3624
Alexander Hartson <alexander-hartson@live.com>
2018-01-26 20:51:19 -0600

    Update chapter 17.cpp

--- Chapter 17/chapter 17.cpp
+++ Chapter 17/chapter 17.cpp
@@ -1,20 +1,14 @@
 #include "std_lib_facilities.h"
 
-//a very simplified vector of doubles (like vector<double>)
-class vector 
-{
-	int sz; //the size
-	double* elem; //point to the first element (of type double)
-public:
-	vector(int s); //constructor: allocate s doubles,
-				   //let elem point to them
-				   //store s in sz
-	int size() const { return sz; } //the current size
-};
-
 void sizes(char ch, int i, int* p)
 {
 	cout << "the size of char is " << sizeof(char) << ' ' << sizeof(ch) << '\n';
 	cout << "the size of int is " << sizeof(int) << ' ' << sizeof(i) << '\n';
 	cout << "the size of int* is " << sizeof(int*) << ' ' << sizeof(p) << '\n';
+}
+
+int main()
+{
+	sizes(char ch, int i, int* p);
+	return 0;
 }
