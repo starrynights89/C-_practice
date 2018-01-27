d5e8deffc5a4804265de2d000f11346106f793d2
Alexander Hartson <alexander-hartson@live.com>
2018-01-26 23:26:23 -0600

    code change

--- Chapter 17/chapter 17.cpp
+++ Chapter 17/chapter 17.cpp
@@ -1,14 +1,13 @@
 #include "std_lib_facilities.h"
 
-void sizes(char ch, int i, int* p)
+int main()
 {
+	char ch;
+	int i;
+	int *p;
 	cout << "the size of char is " << sizeof(char) << ' ' << sizeof(ch) << '\n';
 	cout << "the size of int is " << sizeof(int) << ' ' << sizeof(i) << '\n';
 	cout << "the size of int* is " << sizeof(int*) << ' ' << sizeof(p) << '\n';
-}
-
-int main()
-{
-	sizes(char ch, int i, int* p);
+	keep_window_open();
 	return 0;
 }
