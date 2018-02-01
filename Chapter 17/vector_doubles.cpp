b749c632b233db0c416ddb37325971ca06b76184
Alexander Hartson <alexander-hartson@live.com>
2018-01-31 22:14:01 -0600

    Add files via upload

---
+++ Chapter 17/vector_doubles.cpp
@@ -0,0 +1,26 @@
+#include "std_lib_facilities.h"
+
+//a very simplified vector of doubles
+class vector
+{
+	int sz; //the size
+	double* elm; //a pointer to the elements
+public:
+	vector(int s) :sz{s}, elem{new double[s]} {/*...*/} //constructor
+	~vector() { delete[] elem; } 
+	
+	int size() const { return sz; } //the current size
+	
+	double get(int n) const{ return elem[n]; } //access : read
+	void set(int n, double v) { elem[n]=v; } //access : write
+};
+
+int main()
+{
+	vector v(5);
+	for (int i=0; i<v.size(); i++)
+	{
+		v.set(i,1.1*i);
+		cout << "v[" << i << "]==" << v.get(i) << '\n';
+	}
+}
