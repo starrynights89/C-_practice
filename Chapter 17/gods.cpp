504b862f74dfa6e7b22cd65018b6bbfbacb5d418
Alexander Hartson <alexander-hartson@live.com>
2018-02-14 18:22:04 -0600

    adding code

--- Chapter 17/gods.cpp
+++ Chapter 17/gods.cpp
@@ -1,1 +1,8 @@
 #include "std_lib_facilities.h"
+
+Link* norse_gods = new Link("Thor");
+norse_gods = insert(norse_gods,new Link{"Odin"});
+norse_gods = insert(norse_gods,new Link{"Zeus"});
+norse_gods = insert(norse_gods,new Link{"Freia"});
+
+Link* greek_gods = new Link("Hera");
